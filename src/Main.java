5cbdb8c05d05f576f45f1ee4c57b9e294e823fd8
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-04-16 22:23:27 +0300

    work commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import decorator.NoteDecorators;
+import decorator.Playable;
+import executearound.StudioMicrophone;
+import factorymethod.BasicAir;
+import factorymethod.BasicPro;
+import factorymethod.MacBookStore;
+import strategy.Musician;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("=== 1. FP СТРАТЕГІЯ ===");
+        Musician musician = new Musician();
+
+        // Передаємо лямбда-вирази замість цілих класів стратегій
+        musician.setStrategy((n1, n2) -> System.out.println("Legato FP: " + n1 + " " + n2));
+        musician.performNote("До", "Ре");
+
+        musician.setStrategy((n1, n2) -> System.out.println("Staccato FP: " + n1 + " " + n2));
+        musician.performNote("Мі", "Фа");
+
+
+        System.out.println("\n=== 2. FP ФАБРИЧНИЙ МЕТОД ===");
+        MacBookStore store = new MacBookStore();
+
+        // Передаємо посилання на конструктор (Supplier)
+        store.deliverToStore(BasicAir::new);
+        store.deliverToStore(BasicPro::new);
+
+
+        System.out.println("\n=== 3. FP ДЕКОРАТОР ===");
+        // Базова нота
+        Playable simpleNote = () -> System.out.println("Грає нота: До (C)");
+
+        // Загортаємо ноту в декоратори як матрьошку
+        Playable decoratedNote = NoteDecorators.sharp(NoteDecorators.flat(simpleNote));
+        decoratedNote.play();
+
+
+        System.out.println("\n=== 4. FP НАВКОЛИШНЄ ВИКОНАННЯ (Execute Around) ===");
+        // Ми не турбуємося про включення/виключення мікрофона,
+        // ми передаємо лише те, ЩО треба записати.
+        StudioMicrophone.executeAround(mic -> {
+            mic.recordVoice("Ah-ah-ah!");
+            mic.recordVoice("Test, one, two.");
+            // Навіть якщо тут станеться помилка (Exception), мікрофон безпечно вимкнеться завдяки finally.
+        });
     }
 }
